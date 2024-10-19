728ed1d386d50f90c23a91fae92d73241bdf349d
vusalasalimovaa <salimovavusala7@gmail.com>
2024-10-19 14:37:13 +0400

    higherOrderFunc

--- 15-30DaysOfJavascript/09-Day/higherOrderFunc.js
+++ 15-30DaysOfJavascript/09-Day/higherOrderFunc.js
@@ -74,6 +74,16 @@ console.log(evenNums);
 
 /////////////////////////////////////////////
 
+// const reduceMethod = nums.reduce((oldValue, currentValue) => {
+//   return oldValue + currentValue;
+// }, 0);
+
+const reduceMethod = nums.reduce((acc, current) => acc + current, 0);
+
+console.log(reduceMethod);
+
+//////////////////////////////////////////////////////////
+
 // function sayHello() {
 //   console.log("Hi guys");
 // }
@@ -126,25 +136,25 @@ console.log(oddNums2);
 
 let users = [
   {
-    id: 1,
+    id: 2,
     username: "Vusala",
     age: 20,
     gender: 2,
   },
   {
-    id: 2,
+    id: 1,
     username: "Gunel",
     age: 18,
     gender: 2,
   },
   {
-    id: 3,
+    id: 4,
     username: "Aynur",
     age: 38,
     gender: 2,
   },
   {
-    id: 4,
+    id: 3,
     username: "Xalid",
     age: 50,
     gender: 1,
@@ -166,3 +176,47 @@ const women = users.filter((user) => user.gender === 2);
 const men = users.filter((user) => user.gender === 1);
 console.log(women);
 console.log(men);
+
+//////////////////////////////////////////////
+
+console.log(users.every((user) => user.id === 1));
+console.log(users.every((user) => typeof user.username === "string"));
+
+//////////////////////////////////////////////////////
+
+console.log(users.some((user) => user.id === 2));
+
+///////////////////////////////////////////////
+
+const currentUser = users.find((user) => user.id === 4);
+const currentUser2 = users.findIndex((user) => user.id === 4);
+
+console.log(currentUser);
+console.log(currentUser2);
+
+/////////////////////////////////////////////////
+
+const names = ["Vusala", "Aygun", "Aynur", "Xedice", "Habiba", "Gunel"];
+const numaralar = [1, 4, 7, 2, 3, 6, 9, 5, 13, 22, 12, 10, 20, 3.5, 4.8, 12.6];
+
+// names.sort();
+names.sort((a, b) => a.localeCompare(b));
+names.sort((a, b) => b.localeCompare(a));
+
+numaralar.sort((a, b) => a - b);
+numaralar.sort((a, b) => b - a);
+
+console.log(names);
+console.log(numaralar);
+
+////////////////////////////////////////////////////
+
+// users.sort((a, b) => a.id - b.id);
+users.sort((a, b) => b.id - a.id);
+console.log(users);
+
+// users.sort((a, b) => a.username.localeCompare(b.username));
+// users.sort((a, b) => b.username.localeCompare(a.username));
+// console.log(users);
+
+//////////////////////////////////////////////////
