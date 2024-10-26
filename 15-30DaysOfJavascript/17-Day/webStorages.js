2fa16b881ea7ab34217a17b9a76e585fd4dfc302
vusalasalimovaa <salimovavusala7@gmail.com>
2024-10-27 01:06:38 +0400

    basket

--- 15-30DaysOfJavascript/17-Day/webStorages.js
+++ 15-30DaysOfJavascript/17-Day/webStorages.js
@@ -1,22 +1,22 @@
 // localStorage.setItem("name","vusala")
 
-console.log(localStorage);
-console.log(localStorage.getItem("name"));
+// console.log(localStorage);
+// console.log(localStorage.getItem("name"));
 
-const names = ["vusala","gunel","aynur"]
-const user = {
-    name:"aygun",
-    surname:"salimova"
-}
+// const names = ["vusala","gunel","aynur"]
+// const user = {
+//     name:"aygun",
+//     surname:"salimova"
+// }
 
-console.log(localStorage.setItem("names",JSON.stringify(names)));
-console.log(localStorage.setItem("user",JSON.stringify(user)));
-console.log(localStorage);
+// console.log(localStorage.setItem("names",JSON.stringify(names)));
+// console.log(localStorage.setItem("user",JSON.stringify(user)));
+// console.log(localStorage);
 
-console.log(JSON.parse(localStorage.getItem("user")));
+// console.log(JSON.parse(localStorage.getItem("user")));
 
-localStorage.removeItem("name")
-console.log(localStorage);
+// localStorage.removeItem("name")
+// console.log(localStorage);
 
 // localStorage.clear()
 
@@ -25,11 +25,11 @@ console.log(localStorage);
 
 let basket = JSON.parse(localStorage.getItem("basket")) || []
 
-let products = [
+const products = [
     {
         id:1,
         title:"Asus Tuf Gaming Notebook",
-        price:2400
+        price:2500
     },
     {
         id:2,
@@ -38,37 +38,73 @@ let products = [
     },
     {
         id:3,
-        title:"Iphone 16 pro Max",
-        price:4000
-    },
+        title:"Iphone 14 Pro Max",
+        price:3000
+    }
 ]
 
-function saveToLocalStorage() {
+function setToLocalStorage() {
     localStorage.setItem("basket",JSON.stringify(basket))
 }
 
 function addToBasket(productId) {
-    basket.push(productId)
-    saveToLocalStorage()
+    const basketIndex = basket.findIndex(b => b.productId == productId)
+    // console.log(basketIndex);
+    if(basketIndex > -1){
+        basket[basketIndex] = {
+            ...basket[basketIndex],
+            amount: basket[basketIndex].amount + 1
+        }
+    }else{
+        basket.push({
+            productId,
+            amount:1
+        })
+    }
+    setToLocalStorage()
 }
 
 function removeBasket(productId) {
-    basket = basket.filter(id => id !== productId)
-    saveToLocalStorage()
+    const item = basket.find(b => b.productId == productId)
+    if(item){
+        if(item.amount == 1){
+            basket = basket.filter(p => p.productId !== productId)
+        } else{
+            basket = basket.map(i => {
+                if(i.productId === productId){
+                    i.amount -= 1
+                }
+                return i
+            })
+        }
+    }
+    // console.log(basket);
+    setToLocalStorage()
 }
 
 function totalPrice() {
-    return basket.reduce((prev,productId) => {
-        let product = products.find(p => p.id == productId)
-        return prev += product.price
+    return basket.reduce((prev,basket) => {
+        const product = products.find(p => p.id == basket.productId)
+        return prev += product.price * basket.amount
     },0)
 }
 
-addToBasket(2)
-addToBasket(3)
+// addToBasket(2)
+// addToBasket(3)
+// addToBasket(1)
+// addToBasket(1)
+// addToBasket(1)
+// addToBasket(1)
+// addToBasket(1)
+// addToBasket(1)
+// addToBasket(1)
 
 
+// removeBasket(1)
+// removeBasket(3)
+// removeBasket(2)
+
 console.log(basket);
-removeBasket(2)
-console.log(basket);
+console.log(localStorage);
 console.log(totalPrice());
+// localStorage.clear()
