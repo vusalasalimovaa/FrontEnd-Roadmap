6214bcec774b543b53c5b65f74b5df15d4394e8a
vusalasalimovaa <salimovavusala7@gmail.com>
2024-10-30 04:55:36 +0400

    closure

---
+++ 15-30DaysOfJavascript/19-Day/closure.js
@@ -0,0 +1,36 @@
+function outerFunction() {
+    let count = 0;
+    function innerFunction() {
+        count++
+        return count
+    }
+
+    return innerFunction
+}
+const innerFunc = outerFunction()
+
+console.log(innerFunc())
+////////////////////////////
+
+function outerFunction() {
+    let count = 0;
+    function plusOne() {
+        count++
+        return count
+    }
+    function minusOne() {
+        count--
+        return count
+    }
+
+    return {
+        plusOne:plusOne(),
+        minusOne:minusOne()
+    }
+}
+const innerFuncs = outerFunction()
+
+console.log(innerFuncs.plusOne)
+console.log(innerFuncs.minusOne)
+console.log(innerFuncs.plusOne)
+console.log(innerFuncs.plusOne)
